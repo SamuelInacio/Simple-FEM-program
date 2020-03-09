9e587d82688f08ea5e9638d5ce9b209b35d1352b
PoroTexugo <33752514+PoroTexugo@users.noreply.github.com>
2020-03-09 02:09:52 +0000

    staged changes to compare to master

---
+++ FEMProgram/DataProcessing.m
@@ -0,0 +1,41 @@
+%DataProcessing: computes the length and the angles between the local and
+%                global coordinates of each element
+function [length, gamma, elementType] = DataProcessing(nodes, elements, elementType, dynamicAnalysis)
+
+    % Pre-alocations
+    gamma = zeros(numel(elements));
+    length = zeros(numel(elements));
+
+    % Compute length and gamma
+    for i = 1:length(elements) 
+
+        % Global coordinates
+        coordNode1 = nodes(elements(i,1),1:2);
+        coordNode2 = nodes(elements(i,2),1:2);
+
+        % Length
+        length(i)= norm((coordNode2-coordNode1));
+
+        % Local Coordinates
+        xx = coordNode2(1)-coordNode1(1);
+        yy = coordNode2(2)-coordNode1(2);
+
+        % Angles
+        if xx >= 0
+            gamma(i) = atand(yy/xx);
+            else
+                gamma(i) = (180+atand(yy/xx));
+        end
+    end %% for
+
+    % If a dynamic analysis is required   
+    if dynamicAnalysis == 'Y' 
+        
+        % Transform all elements into beams
+        for i = 1:length(elements)
+            elementType(i) = 'b';
+        end        
+    end
+    
+end %% function
+
