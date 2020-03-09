34cf9505789b8cb4b6095faab060e2d6ff164671
Samuel Inácio <samuel_coroa@hotmail.com>
2020-03-09 03:22:14 +0100

    began refactoring of the DataProcessing function

--- FEMProgram/transformacao_de_dados.m
+++ FEMProgram/transformacao_de_dados.m
@@ -1,13 +1,11 @@
-function [ L, gamma,E_tipo] = transformacao_de_dados(Nos,Elementos,E_tipo,Analise_Dinamica)
-%transformacao_de_dados calcula os comprimentos L, os angulos entre as
-%coordenadas globais e locais de cada elemento
-
-gamma=zeros(length(Elementos));
-L=zeros(length(Elementos));
-for i=1:length(Elementos) %calculo de L e de gamma
-    coord_no1=Nos(Elementos(i,1),1:2);
-    coord_no2=Nos(Elementos(i,2),1:2);
-    L(i)=norm((coord_no2-coord_no1));
+function [ Element_Length, gamma, element_type] = DataProcessing(nodes, element_number ,element_type, dynamic_analisys)
+% Data_Processing calculates beam and link length L, and the angle gamma between local and global element coordinates.
+gamma=zeros(length(element_number));
+Element_Length=zeros(length(element_number));
+for i=1:length(element_number) %calculo de L e de gamma
+    coord_no1=nodes(element_number(i,1),1:2);
+    coord_no2=nodes(element_number(i,2),1:2);
+    Element_Length(i)=norm((coord_no2-coord_no1));
     xx=coord_no2(1)-coord_no1(1);
     yy=coord_no2(2)-coord_no1(2);
     if xx >= 0
@@ -16,9 +14,9 @@ for i=1:length(Elementos) %calculo de L e de gamma
         gamma(i)= (180+atand(yy/xx));
     end
 end
-if Analise_Dinamica == 'S' %transforma todos os Elementos em vigas para realizar a analise dinamica
-    for i=1:length(Elementos)
-        E_tipo(i)='v';
+if dynamic_analisys == 'S' %transforma todos os Elementos em vigas para realizar a analise dinamica
+    for i=1:length(element_number)
+        element_type(i)='v';
     end
 end
 end
